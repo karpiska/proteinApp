3b302090670ba0f38b8945591f673844ea2474db
ascolm <mertascioglu@gmail.com>
2022-04-29 13:16:43 +0200

    sth to look at

---
+++ src/theme.js
@@ -0,0 +1,12 @@
+import { createTheme } from '@mui/material/styles';
+import { green, purple } from '@mui/material/colors';
+
+const theme = createTheme({
+  palette: {
+    secondary: {
+      main: "#fff",
+    },
+  },
+});
+
+export default theme;
