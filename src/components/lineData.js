a77e6b8dbf1de53ac6c9bc463785343ef18628a0
Iveta Karpisova <i_karpisova@wargaming.net>
2022-04-29 10:56:59 +0200

    Second commit

---
+++ src/components/lineData.js
@@ -0,0 +1,274 @@
+const data = [
+    {
+        "id": "japan",
+        "color": "hsl(300, 70%, 50%)",
+        "data": [
+            {
+                "x": "plane",
+                "y": 86
+            },
+            {
+                "x": "helicopter",
+                "y": 8
+            },
+            {
+                "x": "boat",
+                "y": 149
+            },
+            {
+                "x": "train",
+                "y": 156
+            },
+            {
+                "x": "subway",
+                "y": 36
+            },
+            {
+                "x": "bus",
+                "y": 34
+            },
+            {
+                "x": "car",
+                "y": 1
+            },
+            {
+                "x": "moto",
+                "y": 287
+            },
+            {
+                "x": "bicycle",
+                "y": 26
+            },
+            {
+                "x": "horse",
+                "y": 279
+            },
+            {
+                "x": "skateboard",
+                "y": 268
+            },
+            {
+                "x": "others",
+                "y": 16
+            }
+        ]
+    },
+    {
+        "id": "france",
+        "color": "hsl(327, 70%, 50%)",
+        "data": [
+            {
+                "x": "plane",
+                "y": 79
+            },
+            {
+                "x": "helicopter",
+                "y": 221
+            },
+            {
+                "x": "boat",
+                "y": 63
+            },
+            {
+                "x": "train",
+                "y": 96
+            },
+            {
+                "x": "subway",
+                "y": 10
+            },
+            {
+                "x": "bus",
+                "y": 25
+            },
+            {
+                "x": "car",
+                "y": 65
+            },
+            {
+                "x": "moto",
+                "y": 282
+            },
+            {
+                "x": "bicycle",
+                "y": 87
+            },
+            {
+                "x": "horse",
+                "y": 25
+            },
+            {
+                "x": "skateboard",
+                "y": 190
+            },
+            {
+                "x": "others",
+                "y": 113
+            }
+        ]
+    },
+    {
+        "id": "us",
+        "color": "hsl(79, 70%, 50%)",
+        "data": [
+            {
+                "x": "plane",
+                "y": 246
+            },
+            {
+                "x": "helicopter",
+                "y": 9
+            },
+            {
+                "x": "boat",
+                "y": 191
+            },
+            {
+                "x": "train",
+                "y": 59
+            },
+            {
+                "x": "subway",
+                "y": 117
+            },
+            {
+                "x": "bus",
+                "y": 200
+            },
+            {
+                "x": "car",
+                "y": 253
+            },
+            {
+                "x": "moto",
+                "y": 171
+            },
+            {
+                "x": "bicycle",
+                "y": 33
+            },
+            {
+                "x": "horse",
+                "y": 107
+            },
+            {
+                "x": "skateboard",
+                "y": 182
+            },
+            {
+                "x": "others",
+                "y": 233
+            }
+        ]
+    },
+    {
+        "id": "germany",
+        "color": "hsl(246, 70%, 50%)",
+        "data": [
+            {
+                "x": "plane",
+                "y": 17
+            },
+            {
+                "x": "helicopter",
+                "y": 179
+            },
+            {
+                "x": "boat",
+                "y": 253
+            },
+            {
+                "x": "train",
+                "y": 10
+            },
+            {
+                "x": "subway",
+                "y": 83
+            },
+            {
+                "x": "bus",
+                "y": 293
+            },
+            {
+                "x": "car",
+                "y": 229
+            },
+            {
+                "x": "moto",
+                "y": 35
+            },
+            {
+                "x": "bicycle",
+                "y": 214
+            },
+            {
+                "x": "horse",
+                "y": 4
+            },
+            {
+                "x": "skateboard",
+                "y": 157
+            },
+            {
+                "x": "others",
+                "y": 156
+            }
+        ]
+    },
+    {
+        "id": "norway",
+        "color": "hsl(89, 70%, 50%)",
+        "data": [
+            {
+                "x": "plane",
+                "y": 198
+            },
+            {
+                "x": "helicopter",
+                "y": 231
+            },
+            {
+                "x": "boat",
+                "y": 269
+            },
+            {
+                "x": "train",
+                "y": 117
+            },
+            {
+                "x": "subway",
+                "y": 88
+            },
+            {
+                "x": "bus",
+                "y": 179
+            },
+            {
+                "x": "car",
+                "y": 298
+            },
+            {
+                "x": "moto",
+                "y": 190
+            },
+            {
+                "x": "bicycle",
+                "y": 73
+            },
+            {
+                "x": "horse",
+                "y": 271
+            },
+            {
+                "x": "skateboard",
+                "y": 228
+            },
+            {
+                "x": "others",
+                "y": 215
+            }
+        ]
+    }
+]
+
+export default data
