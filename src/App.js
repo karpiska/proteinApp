691635f718eb8000c8d1105547bfdd788967d272
ascolm <mertascioglu@gmail.com>
2022-04-29 14:19:54 +0200

    diet submenu

--- src/App.js
+++ src/App.js
@@ -1,17 +1,63 @@
 import './App.css';
 import BarChart from './components/BarChart'
 import { useState } from 'react';
-import { AppBar, Toolbar, Button, ButtonGroup, Container } from '@mui/material';
+import { AppBar, Toolbar, Button, ButtonGroup, Popper, Menu, MenuItem } from '@mui/material';
 import AnimalChart from "./components/AnimalChart";
+import comboData from './components/combinations';
+
+const categoryNames = comboData.map((item) => item['Category'])
+console.log(categoryNames)
 
 const App = () => {
   const [selectedGraph, setSelectedGraph] = useState('Aminoacids')
 
+  const [anchorEl, setAnchorEl] = useState(null);
+
+  const handleDietClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleDietClose = () => {
+    setAnchorEl(null);
+  }
+
+  const handleDietChoice = (categoryName) => {
+    setSelectedGraph(categoryName);
+    handleDietClose();
+  }
+
+  const open = Boolean(anchorEl);
+  const id = open ? 'simple-popper' : undefined;
+
+  const generateCategoryMenu = () => {
+      return categoryNames.map((category) => {
+        const categoryName = category.replaceAll('_', ' ')
+        return <MenuItem onClick={() => handleDietChoice(category)}>{categoryName}</MenuItem>
+    })
+  }
+
     const renderGraph = () => {
-        if (selectedGraph === 'Aminoacids') {
-            return  <BarChart/>
-        } else {
-            return <AnimalChart/>
+        switch (selectedGraph) {
+            case 'Aminoacids':
+                return  <BarChart/>
+            case 'ALL':
+                return <></>
+            case 'ANIMAL':
+                return <AnimalChart/>
+            case 'MEAT':
+                return <></>
+            case 'CHICKEN':
+                return <></>
+            case 'MEAT_LEGUMES':
+                return <></>
+            case 'LEGUMES':
+                return <></>
+            case 'LEGUMES_GRAIN_NUTS':
+                return <></>
+            case 'NUTS_FRUITS_VEGGIES':
+                return <></>
+            case 'FRUITS_VEGGIES':
+                return <></>
         }
     }
 
@@ -21,7 +67,18 @@ const App = () => {
                 <Toolbar>
                     <ButtonGroup color="secondary" variant="text" aria-label="text button group">
                         <Button onClick={() => setSelectedGraph('Aminoacids')}>Aminoacids</Button>
-                        <Button onClick={() => setSelectedGraph('Diet')}>Diet</Button>
+                        <Button onClick={handleDietClick}>Diet</Button>
+                        <Menu
+                            id="basic-menu"
+                            anchorEl={anchorEl}
+                            open={open}
+                            onClose={handleDietClose}
+                            MenuListProps={{
+                            'aria-labelledby': 'basic-button',
+                            }}
+                        >
+                            {generateCategoryMenu()}
+                        </Menu>
                     </ButtonGroup>
                 </Toolbar>
             </AppBar>
